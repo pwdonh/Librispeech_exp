36c5e7c0014a86f243a7a1a22343d8c3b89db7ce
Peter Donhauser <peter.donhauser@mail.mcgill.ca>
2019-04-01 18:58:56 -0400

    download data

---
+++ data/download_kaldi_data.sh
@@ -0,0 +1,9 @@
+!#/bin/bash/
+
+wget http://kaldi-asr.org/downloads/build/6/trunk/egs/librispeech/s5/data/lang/phones/align_lexicon.txt
+wget http://kaldi-asr.org/downloads/build/6/trunk/egs/librispeech/s5/data/lang/phones/align_lexicon.int
+wget http://kaldi-asr.org/downloads/build/6/trunk/egs/librispeech/s5/data/lang/phones/sets.txt
+wget http://kaldi-asr.org/downloads/build/6/trunk/egs/librispeech/s5/data/lang/phones/sets.int
+wget http://kaldi-asr.org/downloads/build/6/trunk/egs/librispeech/s5/data/lang/words.txt
+wget http://kaldi-asr.org/downloads/build/6/trunk/egs/librispeech/s5/data/lang/phones.txt
+wget http://kaldi-asr.org/downloads/build/6/trunk/egs/librispeech/s5/data/train_clean_100/text
---
+++ data/download_librispeech_data.sh
@@ -0,0 +1,4 @@
+!#/bin/bash/
+
+wget http://www.openslr.org/resources/12/train-clean-100.tar.gz
+tar -xvf train-clean-100.tar.gz
